431c777d0c0679d822d8f29d381eac4ec2c3855e
Logan-coder-1726 <74015844+Logan-coder-1726@users.noreply.github.com>
2021-10-20 12:59:43 -0400

    Add files via upload

---
+++ home_page.js
@@ -0,0 +1,15 @@
+function tv() {
+    location.replace("tv.html")
+  }
+
+  function phone() {
+    location.replace("phone.html")
+  }
+  
+  function bed() {
+    location.replace("bed.html")
+  }
+
+  function sofa() {
+    location.replace("sofa.html")
+  }
